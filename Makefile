6639f36cba9974507ebf15b370e58ad3bf7a4f69
Lawrence Mok <lawrence.mok@amlogic.com>
2021-09-29 10:38:46 -0700

    secpu: fix build [1/1]
    PD#RSP-2226
    
    Problem:
    After moving CA to separate git, it can't build.
    
    Solution:
    Add missing header file
    Add Makefile
    
    Verify:
    TDK_DIR=$PWD/../buildroot_t7/vendor/amlogic/tdk/ \
    ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- make
    
    Change-Id: I5b818fe176141c9fb3ba1c88f9296535b80144a8
    Signed-off-by: Lawrence Mok <lawrence.mok@amlogic.com>

---
+++ Makefile
@@ -0,0 +1,28 @@
+export V?=0
+cur_dir=$(CURDIR)
+
+export ARCH?=arm
+export CROSS_COMPILE?=arm-linux-gnueabihf-
+export TA_CROSS_COMPILE=arm-linux-gnueabihf-
+
+ifeq ($(PLATFORM_TDK_VERSION), 38)
+$(info TDK_VERSION_IS_$(PLATFORM_TDK_VERSION))
+export TDK_DIR ?= $(cur_dir)/../tdk_v3
+else
+$(info TDK_VERSION_IS_$(PLATFORM_TDK_VERSION)_NOT_DEFINED)
+export TDK_DIR ?= $(cur_dir)/../tdk
+endif
+
+OUTPUT_DIR_CA = ../out/ca
+OUTPUT_DIR_LB = ../out/lib
+
+.PHONY: all
+all:
+	$(MAKE) -C lib O=$(OUTPUT_DIR_LB)
+	$(MAKE) -C ca O=$(OUTPUT_DIR_CA)
+
+.PHONY: clean
+clean:
+	$(MAKE) -C ca clean O=$(OUTPUT_DIR_CA)
+	$(MAKE) -C lib clean O=$(OUTPUT_DIR_LB)
+
---
+++ lib/include/ta_secpu_fw_loader.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 Amlogic, Inc. All rights reserved.
+ *
+ * All information contained herein is Amlogic confidential.
+ *
+ * This software is provided to you pursuant to Software License
+ * Agreement (SLA) with Amlogic Inc ("Amlogic"). This software may be
+ * used only in accordance with the terms of this agreement.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification is strictly prohibited without prior written permission
+ * from Amlogic.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TA_SECPU_FW_LOADER_H
+#define TA_SECPU_FW_LOADER_H
+
+/* This UUID is generated with uuidgen
+   the ITU-T UUID generator at http://www.itu.int/ITU-T/asn1/uuid.html */
+#define TA_SECPU_FW_LOADER_UUID { 0x15c8ded6, 0x3b23, 0x48dd, \
+		{0xbe, 0xf2, 0xb2, 0x71, 0xbc, 0xac, 0x96, 0x07} }
+
+/* The TAFs ID implemented in this TA */
+#define TA_CMD_LOAD_SECPU_FW    0
+
+
+#endif /* TA_SECPU_FW_LOADER_H */
